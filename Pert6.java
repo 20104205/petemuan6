a8acae8a53a07c4a87943a3d616b17d8e9ea8042
Raihan Abigail <101303595+20104205@users.noreply.github.com>
2022-04-12 13:33:45 +0800

    Add files via upload

---
+++ Pert6.java
@@ -0,0 +1,19 @@
+
+package pert6latihan01;
+
+/**
+ *
+ * @Raihan Abigail 20104205
+ */
+public class Pert6latihan01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         LuasSegitiga segitiga = new LuasSegitiga(7,6);
+          segitiga.CetakLuas();
+    }
+    
+}
